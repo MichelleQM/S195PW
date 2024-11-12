82aca8c65571d69219fe12a17ccf467054cf8d47
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2024-11-11 19:02:07 -0600

    Practica seeders

---
+++ introLaravel/database/seeders/clientesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use illiminate\Support\Facades\DB;
+
+class clientesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('clientes')->insert([
+            [
+                'nombre' => 'iIvan',
+                'apellido' => 'Guerra',
+                'correo' => 'iiguerra@gmail.com',
+                'telefono' => '1234567890',
+            ],[
+                'nombre' => 'Michelle',
+                'apellido' => 'Quintero',
+                'correo' => 'correo1@gmail.com',
+                'telefono' => '1234567890',
+            ],[
+                'nombre' => 'Maria',
+                'apellido' => 'Lopez',
+                'correo' => 'lopezm@gmail.com',
+                'telefono' => '1234567890',
+            ]
+        ]);
+    }
+}
